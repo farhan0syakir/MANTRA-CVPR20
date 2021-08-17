c38fb33a26f954fc845ad5badce85942485ea32f
Farhan Syakir <farhan0syakir@gmail.com>
2021-08-17 21:42:55 +0300

    fix argument issue

--- train_IRM.py
+++ train_IRM.py
@@ -1,10 +1,10 @@
 import argparse
 from trainer import trainer_IRM
-import torch
+from utils import str2bool
 
 def parse_config():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cuda", default=True)
+    parser.add_argument("--cuda", type=str2bool, default=True)
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--learning_rate", type=int, default=0.0001)
     parser.add_argument("--max_epochs", type=int, default=600)
@@ -13,12 +13,14 @@ def parse_config():
     parser.add_argument("--future_len", type=int, default=40)
     parser.add_argument("--preds", type=int, default=5)
     parser.add_argument("--d_model", type=int, default=512)
+    parser.add_argument("--bride", type=int, default=0)
+    parser.add_argument("--use_irm", type=str2bool, default=True)
 
     # MODEL CONTROLLER
     parser.add_argument("--model", default='pretrained_models/model_controller/model_controller')
 
-    parser.add_argument("--saved_memory", default=True)
-    parser.add_argument("--saveImages", default=True, help="plot qualitative examples in tensorboard")
+    parser.add_argument("--saved_memory", type=str2bool, default=True)
+    parser.add_argument("--saveImages", type=str2bool, default=True, help="plot qualitative examples in tensorboard")
     parser.add_argument("--dataset_file", default="kitti_dataset.json", help="dataset file")
     parser.add_argument("--info", type=str, default='', help='Name of training. '
                                                              'It will be used in tensorboard log and test folder')
