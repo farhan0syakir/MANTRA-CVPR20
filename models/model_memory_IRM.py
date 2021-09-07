402f221b5ce1b21226c13e73b3e4b737a4000f30
Farhan Syakir <farhan0syakir@gmail.com>
2021-09-07 19:24:32 +0300

    align_corners=False

--- models/model_memory_IRM.py
+++ models/model_memory_IRM.py
@@ -189,7 +189,7 @@ class model_memory_IRM(nn.Module):
                 indices = pred_map.permute(0, 2, 1, 3)
                 # rescale between -1 and 1
                 indices = 2 * (indices / 180) - 1
-                output = F.grid_sample(scene_2, indices, mode='nearest', align_corners=True)
+                output = F.grid_sample(scene_2, indices, mode='nearest', align_corners=False)
                 output = output.squeeze(2).permute(0, 2, 1)
                 output = output.transpose(0, 1)
                 output = self.multihead_irm(output)
